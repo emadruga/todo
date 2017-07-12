5c39282e5c58c40d49fe37d7fd475745ea8be094
Ewerton Madruga <emadruga@gmail.com>
2017-07-12 11:12:42 -0300

    Completed lesson 6

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { EditTodoPage } from '../edit-todo/edit-todo';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    
+    todo: any;
+    todos: any = [];
+    
+    constructor(public navCtrl: NavController) {
+	this.todo = {
+	    title: '',
+	    description: ''
+	};
+	this.todos = [
+	    {title: 'Todo 1', description: 'abc'},
+	    {title: 'Todo 2', description: 'abc'},
+	    {title: 'Todo 3', description: 'abc'},
+	    {title: 'Todo 4', description: 'abc'},
+	    {title: 'Todo 5', description: 'abc'},
+	    {title: 'Todo 6', description: 'abc'},
+	    {title: 'Todo 7', description: 'abc'},
+	    {title: 'Todo 8', description: 'abc'},
+	];
 
-  }
+    }
+
+    ionViewDidLoad() {
+	
+    }
+
+    save() {
+	console.log('TODO: finish save function');
+    }
+
+    addTodo(){
+	console.log('TODO: finish add function');
+	this.navCtrl.push(EditTodoPage);
+    }
+
+    editTodo(todo){
+	console.log(todo);
+	this.navCtrl.push(EditTodoPage, {
+	    todo: todo
+	});
+    }
+
 
 }
